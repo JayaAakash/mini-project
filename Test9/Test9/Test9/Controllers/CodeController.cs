d7641566d47b0e84ff819e422c4ea1efd4198a7e
JayaAakash <160754826+JayaAakash@users.noreply.github.com>
2024-04-28 23:37:16 -0700

    Add files via upload

---
+++ Test9/Test9/Test9/Controllers/CodeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Test9.Models; // Replace YourNamespace with your project's namespace
+
+public class CodeController : Controller
+{
+    private NorthwindEntities db = new NorthwindEntities(); // Use your DbContext class name
+
+    public ActionResult CustomersInGermany()
+    {
+        var customersInGermany = db.Customers.Where(c => c.Country == "Germany").ToList();
+        return View(customersInGermany);
+    }
+
+    public ActionResult CustomerWithOrderId(int orderId)
+    {
+        var customer = db.Customers.FirstOrDefault(c => c.Orders.Any(o => o.OrderID == orderId));
+        return View(customer);
+    }
+}
